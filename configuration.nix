4648978f3bb0b5afe9cb9867c221336c4d1a3293
Alex Stone <stonealex145@gmail.com>
2026-08-20 17:18:41 -0400

    Added a nix configuration

---
+++ configuration.nix
@@ -0,0 +1,104 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.useOSProber = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.hostName = "nixos-workstation"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  # Timezones and Locales
+  time.timeZone = "America/New_York";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Desktop Environment
+  services.xserver.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+  programs.dconf.enable = true;
+
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-tour
+  ]);
+
+
+  # Enable CUPS Printing
+  services.printing.enable = true;
+
+  # Enable Pipewire
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  users.users."alex" = {
+    isNormalUser = true;
+    description = "Alex Stone";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      kdePackages.kate
+    ];
+  };
+
+  # Firefox here for some reason?
+  programs.firefox.enable = true;
+
+  # Packages
+  nixpkgs.config.allowUnfree = true;
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-color-emoji
+    nerd-fonts.fira-code
+    nerd-fonts.jetbrains-mono
+  ];
+  environment.systemPackages = with pkgs; [
+    # Gui Apps
+    gnome-tweaks
+    adwaita-icon-theme
+    chromium
+    # Tooling
+    neovim
+    emacs
+    tmux
+    git
+    ripgrep
+    htop
+    # Languages
+    rustc
+    cargo
+    rust-analyzer
+    python3
+    pyright
+    zig
+    zls
+    clang-tools
+    gcc
+  ];
+
+  system.stateVersion = "26.05";
+}
